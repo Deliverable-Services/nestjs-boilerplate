d978296e772d4c13da9e1324baf5bc218149f6c0
Sumit <sumitrana1996@gmail.com>
2021-12-24 18:37:10 +0530

    update

--- src/app.module.js
+++ src/app.module.js
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [CacheModule.register(), MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
